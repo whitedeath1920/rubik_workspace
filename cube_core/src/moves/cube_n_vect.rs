1fb55e3d95b412350df855360b1ac8140dda1627
Francisco Alas <franciscodavidalaslopez@gmail.com>
2026-02-01 10:01:40 -0600

    added pattern for 0, 1 and 2

--- cube_core/src/moves/cube_n_vect.rs
+++ cube_core/src/moves/cube_n_vect.rs
@@ -4,11 +4,11 @@ use std::{collections::HashMap, ops::Range};
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Orbit {
     Corners(i32) = 0,
-    Edges(i32)= 1,
-    Centers(i32)= 2,
-    ParCenters(i32)= 3,
-    ParEdges(i32)= 4,
-    EdgeCenters(i32)= 5,
+    Edges(i32) = 1,
+    Centers(i32) = 2,
+    ParCenters(i32) = 3,
+    ParEdges(i32) = 4,
+    EdgeCenters(i32) = 5,
 }
 impl Orbit {
     #[inline(always)]
@@ -24,12 +24,23 @@ impl Orbit {
     }
     pub fn to_string(&self) -> String {
         match self {
-            Orbit::Corners(i) => format!("Corners:{}",*i),
-            Orbit::Edges(i) => format!("Edges:{}",*i),
-            Orbit::Centers(i) => format!("Centers:{}",*i),
-            Orbit::ParCenters(i) => format!("ParCenters:{}",*i),
-            Orbit::ParEdges(i) => format!("ParEdges:{}",*i),
-            Orbit::EdgeCenters(i) => format!("EdgeCenters:{}",*i),
+            Orbit::Corners(i) => format!("Corners:{}", *i),
+            Orbit::Edges(i) => format!("Edges:{}", *i),
+            Orbit::Centers(i) => format!("Centers:{}", *i),
+            Orbit::ParCenters(i) => format!("ParCenters:{}", *i),
+            Orbit::ParEdges(i) => format!("ParEdges:{}", *i),
+            Orbit::EdgeCenters(i) => format!("EdgeCenters:{}", *i),
+        }
+    }
+    fn from_idx(kind: usize, idx: i32) -> Self {
+        match kind {
+            0 => Orbit::Corners(idx),
+            1 => Orbit::Edges(idx),
+            2 => Orbit::Centers(idx),
+            3 => Orbit::ParCenters(idx),
+            4 => Orbit::ParEdges(idx),
+            5 => Orbit::EdgeCenters(idx),
+            _ => panic!("Invalid orbit index"),
         }
     }
 }
@@ -79,13 +90,39 @@ struct State(Orbit, i32);
 pub struct CubeVect(HashMap<Point, State>);
 impl CubeVect {
     pub fn to_string(&self) -> String {
-        self.0.iter().map(|(p, s)| format!("({}, {}, {}):({}, {})\n", p.0, p.1, p.2, s.0.to_string(), s.1)).collect::<Vec<_>>().join(", ")
+        self.0
+            .iter()
+            .map(|(p, s)| {
+                format!(
+                    "({}, {}, {}):({}, {})\n",
+                    p.0,
+                    p.1,
+                    p.2,
+                    s.0.to_string(),
+                    s.1
+                )
+            })
+            .collect::<Vec<_>>()
+            .join(", ")
     }
 }
 pub struct CubePerm(HashMap<State, Point>);
 impl CubePerm {
     pub fn to_string(&self) -> String {
-        self.0.iter().map(|(s, p)| format!("({}, {}):({}, {}, {})\n", s.0.to_string(), s.1, p.0, p.1, p.2)).collect::<Vec<_>>().join(", ")
+        self.0
+            .iter()
+            .map(|(s, p)| {
+                format!(
+                    "({}, {}):({}, {}, {})\n",
+                    s.0.to_string(),
+                    s.1,
+                    p.0,
+                    p.1,
+                    p.2
+                )
+            })
+            .collect::<Vec<_>>()
+            .join(", ")
     }
 }
 pub struct CubePair {
@@ -120,50 +157,107 @@ impl CubePair {
         println!("Permutation: {}", self.perm.to_string());
     }
 }
-fn map_perm_vect(dimension: usize, kind: usize, index: usize, arr: &mut Vec<(Point, State)> ) {
+fn map_perm_vect(dimension: usize, kind: usize, index: usize, arr: &mut Vec<(Point, State)>) {
     arr.clear();
     let dimension = dimension as i32;
     let index = index as i32;
     match kind {
-        0 => _sub_0(dimension, index, arr),
-        1 => _sub_1(dimension, index, arr),
-        2 => (),
-        3 => (),
+        // 0 => _sub_0(dimension, kind, index, arr),
+        // 1 => _sub_1(dimension, kind, index, arr),
+        // 2 => _sub_2(dimension, kind, index, arr),
+        3 => _sub_3(dimension, kind, index, arr),
         4 => (),
         5 => (),
-        _ => unreachable!("Invalid kind"),
+        _ => (),
+        // _ => unreachable!("Invalid kind"),
     };
 }
-fn pattern_1(x: i32, y: i32, z: i32, idx: i32, start: i32) -> [(Point, State); 4] {
+fn pattern_1(x: i32, y: i32, z: i32, kind: usize, idx: i32, start: i32) -> [(Point, State); 4] {
     [
-        (Point(-x, y, -z), State(Orbit::Corners(idx), start)),
-        (Point(-z, y + 1, z), State(Orbit::Corners(idx), start+1)),
-        (Point(x + 1, y, z), State(Orbit::Corners(idx), start+2)),
-        (Point(x + 1, y + 1, z), State(Orbit::Corners(idx), start+3)),
+        (Point(-x, y, -z), State(Orbit::from_idx(kind, idx), start)),
+        (
+            Point(-z, y, x),
+            State(Orbit::from_idx(kind, idx), start + 1),
+        ),
+        (Point(x, y, z), State(Orbit::from_idx(kind, idx), start + 2)),
+        (
+            Point(z, y, -x),
+            State(Orbit::from_idx(kind, idx), start + 3),
+        ),
     ]
 }
-fn pattern_2(x: i32, y: i32, z: i32, idx: i32, start: i32) -> [(Point, State); 8] {
+fn pattern_2(x: i32, y: i32, z: i32, kind: usize, idx: i32, start: i32) -> [(Point, State); 8] {
     [
-        (Point(-x, y, -z), State(Orbit::Corners(idx), start)),
-        (Point(-z, y, -x), State(Orbit::Corners(idx), start+1)),
-        (Point(-z, y, x), State(Orbit::Corners(idx), start+2)),
-        (Point(-x, y, z), State(Orbit::Corners(idx), start+3)),
-        (Point(x, y, z), State(Orbit::Corners(idx), start+4)),
-        (Point(z, y, x), State(Orbit::Corners(idx), start+5)),
-        (Point(z, y, -x), State(Orbit::Corners(idx), start+6)),
-        (Point(x, y, -z), State(Orbit::Corners(idx), start+7)),
+        (Point(-x, y, -z), State(Orbit::from_idx(kind, idx), start)),
+        (
+            Point(-z, y, -x),
+            State(Orbit::from_idx(kind, idx), start + 1),
+        ),
+        (
+            Point(-z, y, x),
+            State(Orbit::from_idx(kind, idx), start + 2),
+        ),
+        (
+            Point(-x, y, z),
+            State(Orbit::from_idx(kind, idx), start + 3),
+        ),
+        (Point(x, y, z), State(Orbit::from_idx(kind, idx), start + 4)),
+        (Point(z, y, x), State(Orbit::from_idx(kind, idx), start + 5)),
+        (
+            Point(z, y, -x),
+            State(Orbit::from_idx(kind, idx), start + 6),
+        ),
+        (
+            Point(x, y, -z),
+            State(Orbit::from_idx(kind, idx), start + 7),
+        ),
     ]
 }
-fn _sub_0(dimension: i32, idx: i32, arr: &mut Vec<(Point, State)>) {
-    let n = dimension  >> 1; 
-    arr.extend_from_slice(&pattern_1(n, -n, n, idx,0));
-    arr.extend_from_slice(&pattern_1(n, n, n, idx,4));
+fn _sub_0(dimension: i32, kind: usize, idx: i32, arr: &mut Vec<(Point, State)>) {
+    let n = dimension >> 1;
+    arr.extend_from_slice(&pattern_1(n, -n, n, kind, idx, 0));
+    arr.extend_from_slice(&pattern_1(n, n, n, kind, idx, 4));
 }
-fn _sub_1(dimension: i32, idx: i32, arr: &mut Vec<(Point, State)>) {
-    let n = dimension  >> 1; 
-    arr.extend_from_slice(&pattern_1(0, -n, n, idx,0));
-    arr.extend_from_slice(&pattern_1(n, 0, n, idx,4));
-    arr.extend_from_slice(&pattern_1(0, n, n, idx,4));
+fn _sub_1(dimension: i32, kind: usize, idx: i32, arr: &mut Vec<(Point, State)>) {
+    let n = dimension >> 1;
+    arr.extend_from_slice(&pattern_1(0, -n, n, kind, idx, 0));
+    arr.extend_from_slice(&pattern_1(n, 0, n, kind, idx, 4));
+    arr.extend_from_slice(&pattern_1(0, n, n, kind, idx, 8));
+}
+fn _sub_2(dimension: i32, kind: usize, idx: i32, arr: &mut Vec<(Point, State)>) {
+    let n = dimension >> 1;
+    arr.extend_from_slice(&[
+        (Point(0, -n, 0), State(Orbit::from_idx(kind, idx), 0)),
+        (Point(0, 0, -n), State(Orbit::from_idx(kind, idx), 1)),
+        (Point(-n, 0, 0), State(Orbit::from_idx(kind, idx), 2)),
+        (Point(0, 0, n), State(Orbit::from_idx(kind, idx), 3)),
+        (Point(n, 0, 0), State(Orbit::from_idx(kind, idx), 4)),
+        (Point(0, n, 0), State(Orbit::from_idx(kind, idx), 5)),
+    ]);
+}
+fn _sub_3(dimension: i32, kind: usize, idx: i32, arr: &mut Vec<(Point, State)>) {
+    let n = dimension >> 1;
+    let tmp = (dimension - 2 - (dimension % 2)) / 2;
+    let x = n - (idx) % tmp - 1;
+    let z = n - 1 - (idx - idx % tmp) / tmp;
+
+    arr.extend_from_slice(&pattern_1(x, -n, z, kind, idx, 0));
+    if z == x {
+        arr.extend_from_slice(&pattern_2(z, -z, n, kind, idx, 1));
+        arr.extend_from_slice(&pattern_2(z, z, n, kind, idx, 1));
+    } else if z > x {
+        arr.extend_from_slice(&pattern_1(n, -z, x, kind, idx, 1));
+        arr.extend_from_slice(&pattern_1(z, -x, n, kind, idx, 1));
+        arr.extend_from_slice(&pattern_1(n, x, z, kind, idx, 1));
+        arr.extend_from_slice(&pattern_1(x, z, n, kind, idx, 1));
+    } else {
+        arr.extend_from_slice(&pattern_1(z, -x, n, kind, idx, 1));
+        arr.extend_from_slice(&pattern_1(n, -z, x, kind, idx, 1));
+        arr.extend_from_slice(&pattern_1(x, z, n, kind, idx, 1));
+        arr.extend_from_slice(&pattern_1(n, x, z, kind, idx, 1));
+    }
+    arr.extend_from_slice(&pattern_1(x, n, z,kind,idx,20));
+
 }
 fn number_of_cubies(dimension: usize) -> usize {
     dimension.pow(3) - (dimension - 2).pow(3)
