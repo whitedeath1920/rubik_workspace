377fcede54edf349f3b94cbe4310d8871e82a9bf
fdal-acer <franciscodavidalaslopez@gmail.com>
2026-05-18 12:00:21 -0600

    Update cube_core\src\state\mod.rs added gcd

--- cube_core/src/state/mod.rs
+++ cube_core/src/state/mod.rs
@@ -2,4 +2,4 @@ mod state;
 mod ops;
 
 pub use state::{CubeState, KIND, NUM_PER_KIND, NUM_PER_UNIQUE, IDENTITY_PERM, IDENTITY_ORD, ORI_MOD, PERM_MOD, NUM_KIND};
-pub use ops::{Bit, mcm};
+pub use ops::{Bit, mcm, gcd};
