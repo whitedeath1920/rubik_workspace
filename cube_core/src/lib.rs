86f1d9721633cbe30292167996a812a52c3b79f5
fdal-acer <franciscodavidalaslopez@gmail.com>
2026-08-10 13:35:54 -0600

    current api

--- cube_core/src/lib.rs
+++ cube_core/src/lib.rs
@@ -2,9 +2,12 @@
 //! this includes generating a functional cube and implementing the basic operations
 //! creating the moveset from some rules
 //! and implementing a fast cache `Pool` for especific operations
-// mod alg;
-mod error;
-// pub mod moves;
-pub mod state;
+// #![warn(warnings)]
+pub mod arena;
+pub mod cube_moves;
+pub mod cube_vect;
+pub mod error;
 
+pub use arena::CubeArena;
+pub use cube_vect::{CubeVect, Point3, get_dim_from_len};
 pub use error::CubeError;
